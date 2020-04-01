dae4cdd2f546d2bfd76d8169b95ed191a6acc21e
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-04-01 10:29:33 +0400

    implemented player sprite

--- game/main.py
+++ game/main.py
@@ -1,6 +1,8 @@
 #modules and imports
 import sys
+import os
 import random
+from math import atan2, degrees
 import pygame
 
 # initializer
@@ -9,7 +11,7 @@ pygame.mixer.init()
 
 # window size
 g_width = 1100
-g_height = 600
+g_height = 800
 
 # framerate
 fps = 60
@@ -18,6 +20,7 @@ clock = pygame.time.Clock()
 # colors
 white = (255, 255, 255)
 black = (0, 0, 0)
+gray = (170, 170, 170)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
@@ -29,29 +32,25 @@ pygame.display.set_caption('Pew Pew - Test Build')
 
 class Player(pygame.sprite.Sprite):
     # sprite for the player
-    def __init__(self):
+    def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(white)
+        sizex = 170
+        sizey = sizex // 2
+        self.image = pygame.transform.scale(
+            pygame.image.load(os.path.join("data", "sprites", "player.png")),
+            (sizex, sizey)
+        )
+        # self.image.fill(white)
         self.rect = self.image.get_rect()
-        self.rect.center = (g_width / 2, g_height / 2)
+        self.rect.center = pos
         self.speedx = 0
         self.speedy = 0
+        self.hdg = 100
 
     def update(self):
-        self.speedx = 0
-        self.speedy = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_a]:
-            self.speedx = -4
-        if keystate[pygame.K_d]:
-            self.speedx = 4
         self.rect.x += self.speedx
-        if keystate[pygame.K_w]:
-            self.speedy = -4
-        if keystate[pygame.K_s]:
-            self.speedy = 4
         self.rect.y += self.speedy
+
         #constraints / boundaries
         if self.rect.right > g_width:
             self.rect.right = g_width
@@ -62,10 +61,31 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom > g_height:
             self.rect.bottom = g_height
 
+    def draw(self):
+        img = pygame.transform.rotate(self.image, -self.hdg)
+        rect = img.get_rect()
+        pos = (self.rect.x - rect.width / 2, self.rect.y - rect.height / 2)
+        g.blit(img, pos)
+        self.rect.width, self.rect.height = rect.width, rect.height
 
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+
+def get_angle(pos1, pos2):
+    """
+    Get the angle between 2 points
+
+    Arguments:
+        pos1 {int} -- position of 1st object
+        pos2 {int} -- position of 2nd object
+    """
+
+    dx = pos1[0] - pos2[0]
+    dy = pos1[1] - pos2[1]
+    return degrees(atan2(dy, dx))
+
+
+all_sprites = []
+player = Player((g_width / 2, g_height / 2))
+all_sprites.append(player)
 
 
 # game loop
@@ -78,11 +98,31 @@ while True:
             pygame.quit()
             sys.exit()
     # update
-    all_sprites.update()
+    mouse = pygame.mouse.get_pos()
+    keystate = pygame.key.get_pressed()
+    speed = 4
+
+    if keystate[pygame.K_a]:
+        player.speedx = -speed
+    elif keystate[pygame.K_d]:
+        player.speedx = speed
+    else:
+        player.speedx = 0
+
+    if keystate[pygame.K_w]:
+        player.speedy = -speed
+    elif keystate[pygame.K_s]:
+        player.speedy = speed
+    else:
+        player.speedy = 0
+
+    player.hdg = get_angle(mouse, (player.rect.x, player.rect.y))
+
+    for sprite in all_sprites:
+        sprite.update()
     #draw / render
-    g.fill(black)
-    all_sprites.draw(g)
+    g.fill(gray)
+    for sprite in all_sprites:
+        sprite.draw()
     # *after* drawing everything, flip the display
     pygame.display.flip()
-
-pygame.quit()
